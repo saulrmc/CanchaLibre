981dc3b6ac0f4bfdd819d32b940e5323f49dad08
aortiz356 <a20212456@pucp.edu.pe>
2026-04-07 23:46:14 -0500

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -1,7 +1,7 @@
 import java.time.LocalDateTime;
 class Reserva {
     private int idReserva;
-    private DateTime fechaHora; // Original: DateTime (En Java real sería LocalDateTime)
+    private DateTime fechaHora; 
     private int duracion;
     private EstadoReserva estado;
 }
