4b66e6643e67c66a291f03c19c89f6a20769c478
kaozz <kroncalt@gmail.com>
2026-04-20 18:40:13 -0500

    edit resena

--- CanchaLibreModelo/src/main/java/pe/edu/pucp/CanchaLibre/modelo/Resena/Resena.java
+++ CanchaLibreModelo/src/main/java/pe/edu/pucp/CanchaLibre/modelo/Resena/Resena.java
@@ -1,43 +1,8 @@
 package pe.edu.pucp.CanchaLibre.modelo.Resena;
 public class Resena{
 	private int idUsuario;
-	private String nombres;
-	private String contrasena;
-	private String correo;
-	private String telefono;
+	private int calificacion;
 	private String descripcion;
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public String getCorreo() {
-		return correo;
-	}
-
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-
-	public String getContrasena() {
-		return contrasena;
-	}
-
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-
-	public String getNombres() {
-		return nombres;
-	}
-
-	public void setNombres(String nombres) {
-		this.nombres = nombres;
-	}
 
 	public int getIdUsuario() {
 		return idUsuario;
@@ -45,6 +10,14 @@ public class Resena{
 
 	public void setIdUsuario(int idUsuario) {
 		this.idUsuario = idUsuario;
+	}
+
+	public int getCalificacion() {
+		return calificacion;
+	}
+
+	public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
 	}
 
 	public String getDescripcion() {
