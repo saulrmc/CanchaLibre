3b19887879c1650856ab80ebd4e666ff23e09586
kaozz <kroncalt@gmail.com>
2026-04-21 18:06:01 -0500

    ###

--- CanchaLibrePersistencia/src/main/java/pe/edu/pucp/CanchaLibre/dao/cancha/CanchaDAOImpl.java
+++ CanchaLibrePersistencia/src/main/java/pe/edu/pucp/CanchaLibre/dao/cancha/CanchaDAOImpl.java
@@ -5,9 +5,65 @@ import pe.edu.pucp.CanchaLibre.modelo.Cancha.Cancha;
 
 import java.sql.*;
 
-public class CanchaDAOImpl {
+public class CanchaDAOImpl extends DefaultBaseDAO<Cancha> implements CanchaDAO {
+    protected PreparedStatement comandoCrear(Connection conn,
+                                             Cancha modelo) throws SQLException{
+        String sql = """
+            INSERT INTO CANCHA (
+                idCancha,
+                nombre,
+                descripcion,
+                imagenUrl,
+                disponible,
+                direccion
+            ) VALUES (?, ?, ?, ?, ?, ?)
+        """;
 
-}
+        PreparedStatement cmd = conn.prepareStatement(sql,
+                Statement.RETURN_GENERATED_KEYS);
+        cmd.setInt(1,modelo.getIdCancha());
+        cmd.setString(2,modelo.getNombre());
+        cmd.setString(3,modelo.getDescripcion());
+        cmd.setString(4,modelo.getImagenUrl());
+        cmd.setBoolean(5,modelo.isDisponible());
+        cmd.setString(6,modelo.getDireccion());
+
+        return cmd;
+    }
+
+    protected PreparedStatement comandoActualizar(Connection conn,
+                                                  Cancha modelo) throws SQLException{
+        String sql = """
+            UPDATE CANCHA
+            SET nombre = ?,
+                descripcion = ?,
+                imagenUrl = ?,
+                disponible = ?,
+                direccion = ?
+            WHERE idCancha = ?
+            """;
+
+        PreparedStatement cmd = conn.prepareStatement(sql);
+        cmd.setString(1, modelo.getNombre());
+        cmd.setString(2, modelo.getDescripcion());
+        cmd.setString(3, modelo.getImagenUrl());
+        cmd.setBoolean(4, modelo.isDisponible());
+        cmd.setString(5, modelo.getDireccion());
+        cmd.setInt(6, modelo.getIdCancha());
+
+        return cmd;
+    }
+
+    protected PreparedStatement comandoEliminar(Connection conn,
+                                                Integer id) throws SQLException{
+        String sql = """
+                DELETE FROM CANCHA WHERE idCancha = ?
+                """;
+        PreparedStatement cmd = conn.prepareStatement(sql);
+        cmd.setInt(1,id);
+
+        return cmd;
+    }
 
     protected PreparedStatement comandoLeer(Connection conn,
                                             Integer id) throws SQLException{
