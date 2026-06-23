16aa3c160f017ba4ba6cf7b8156635a78c3a18d3
kaozz <kroncalt@gmail.com>
2026-06-23 12:13:20 -0500

    Update NotificacionBloqueo

--- BackEnd/CanchaLibre/CanchaLibreModelo/src/main/java/pe/edu/pucp/canchalibre/modelo/notificacion/NotificacionBloqueo.java
+++ BackEnd/CanchaLibre/CanchaLibreModelo/src/main/java/pe/edu/pucp/canchalibre/modelo/notificacion/NotificacionBloqueo.java
@@ -1,15 +1,6 @@
 package pe.edu.pucp.canchalibre.modelo.notificacion;
 
-import pe.edu.pucp.canchalibre.modelo.usuario.CuentaUsuario;
-
 public class NotificacionBloqueo extends Notificacion{
     // cuando la cuenta se bloquea se le debe enviar un correo
     // electrónico al usuario
-    private CuentaUsuario cuentaUsuario;
-    public CuentaUsuario getCuentaUsuario() {
-        return cuentaUsuario;
-    }
-    public void setCuentaUsuario(CuentaUsuario cuentaUsuario) {
-        this.cuentaUsuario = cuentaUsuario;
-    }
 }
