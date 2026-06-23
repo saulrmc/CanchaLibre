7b8bb8f74080de0a177ecfb67bf51146249536fd
kaozz <kroncalt@gmail.com>
2026-06-23 18:41:46 -0500

    Delete BloqueHorario: Negocio / Resources

--- BackEnd/CanchaLibre/CanchaLibreNegocio/src/main/java/pe/edu/pucp/canchalibre/bo/cancha/BloqueHorarioBO.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.pucp.canchalibre.bo.cancha;
-
-import pe.edu.pucp.canchalibre.bo.Gestionable;
-import pe.edu.pucp.canchalibre.modelo.cancha.BloqueHorario;
-
-public interface BloqueHorarioBO extends Gestionable<BloqueHorario> {
-
-}
