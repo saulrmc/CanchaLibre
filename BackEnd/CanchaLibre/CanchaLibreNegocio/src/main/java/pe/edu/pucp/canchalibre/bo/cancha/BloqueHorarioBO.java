0615bc83f7b22093add5fe12b312bcb7214ae37f
alejandro <a20212456@pucp.edu.pe>
2026-06-23 22:51:44 -0500

    cambios para el login

---
+++ BackEnd/CanchaLibre/CanchaLibreNegocio/src/main/java/pe/edu/pucp/canchalibre/bo/cancha/BloqueHorarioBO.java
@@ -0,0 +1,9 @@
+package pe.edu.pucp.canchalibre.bo.cancha;
+
+
+import pe.edu.pucp.canchalibre.bo.Gestionable;
+import pe.edu.pucp.canchalibre.modelo.cancha.BloqueHorario;
+
+public interface BloqueHorarioBO extends Gestionable<BloqueHorario> {
+
+}
