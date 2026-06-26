f429083071c70993274b6e4f349ff2d7f748b62c
kaozz <kroncalt@gmail.com>
2026-06-26 11:57:55 -0500

    Backend cleanse

--- BackEnd/CanchaLibre/CanchaLibreModelo/src/main/java/pe/edu/pucp/canchalibre/modelo/notificacion/NotificacionBloqueo.java
+++
@@ -1,6 +0,0 @@
-package pe.edu.pucp.canchalibre.modelo.notificacion;
-
-public class NotificacionBloqueo extends Notificacion{
-    // cuando la cuenta se bloquea se le debe enviar un correo
-    // electrónico al usuario
-}
